7b40148ea3642a8cb51575503279cdd5b675002f
samue <samue@LAPTOP-O0VGNTCP>
2022-07-11 13:52:17 -0300

    Add project files.

---
+++ PRE-FINAL/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRE_FINAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double auto1, auto2, auto3;
+            auto1 = 1000;
+            auto2 = 1500;
+            auto3 = 1750;
+
+            bool seguro, AA;
+            int seleccion = 0;
+            double subTotal = 0;
+            double total = 0;
+            seguro = false;
+            AA = false;
+
+            int opcion = 0;
+            int dias = 0;
+
+            Console.WriteLine("Bienvenido a la agencia de autos");
+            Console.WriteLine("Presione enter para continuar: ");
+            Console.ReadKey();
+            Console.Clear();
+
+            while (opcion != 2)
+            {
+                Console.WriteLine("Seleccione una opcion de vehiculo:");
+                Console.WriteLine($"1- Auto 2 puertas por dia a ${auto1}");
+                Console.WriteLine($"2- Auto 5 puertas por dia a ${auto2}");
+                Console.WriteLine($"3- Camioneta familiar por dia a ${auto3}");
+                seleccion = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+
+                Console.WriteLine("Ingrese la cantidad de días (numeros)");
+                dias = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                if (seleccion == 1)
+                {
+                    Console.WriteLine("Este vehiculo puede no tener aire acondicionado");
+                    Console.WriteLine("Escriba 1 para si desea agregar o 2 si no (25% descuento en caso de no querer)");
+                    if (Console.ReadLine() == "1")
+                    {
+                        AA = true;
+                    }
+                    else
+                    {
+                        AA = false;
+                    }
+                }
+                Console.Clear();
+                Console.WriteLine("Desea agregar un seguro? (10% sobre el monto inicial) 1- Si / 2- No");
+
+                if (Console.ReadLine() == "1")
+                {
+                    seguro = true;
+                }
+                Console.Clear();
+                switch (seleccion)
+                {
+                    case 1:
+                        subTotal += auto1;
+                        break;
+                    case 2:
+                        subTotal += auto2;
+                        break;
+                    case 3:
+                        subTotal += auto3;
+                        break;
+                }
+                if (AA == false & seleccion == 1)
+                {
+
+                    subTotal += subTotal * 0.75;
+                }
+                if (seguro == true)
+                {
+                    subTotal += subTotal * 1.10;
+                }
+
+                Console.Write("Desea Agregar otro vehiculo? \n1- Si\n2- No\n");
+                opcion = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+            }
+            total = subTotal * dias;
+            Console.WriteLine($"Total = {total}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TP DEL PRIMER PARCIAL (BIEN HECHO)/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_DEL_PRIMER_PARCIAL__BIEN_HECHO_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = 0;
+            int continuar = 0;
+            int mat1, mat2, mat3, mat4;
+            mat1 = 3500;
+            mat2 = 2800;
+            mat3 = 1600;
+            mat4 = 1700;
+            double subTotal = 0;
+            double total = 0;
+            bool descuento = false;
+
+
+            while (continuar != 2)
+            {
+                continuar = 0;
+                opcion = 0;
+                subTotal = 0;
+                total = 0;
+                descuento = false;
+                
+                while (continuar != 2)
+                {
+                    Console.WriteLine("Seleccione la materia en la que desea incribirse: ");
+                    Console.WriteLine($"1- Programación Orientada a Objetos ${mat1}");
+                    Console.WriteLine($"2- Programación Visual ${mat2}");
+                    Console.WriteLine($"3- Sistemas Operativos ${mat3}");
+                    Console.WriteLine($"4- Teleinformática ${mat4}");
+                    opcion = Convert.ToInt16(Console.ReadLine());
+                    Console.Clear();
+
+                    Console.WriteLine("Seleccion Exitosa");
+                    Console.ReadKey();
+                    Console.Clear();
+
+                    switch (opcion)
+                    {
+                        case 1:
+                            subTotal += mat1;
+                            break;
+                        case 2:
+                            subTotal += mat2;
+                            break;
+                        case 3:
+                            subTotal += mat3;
+                            break;
+                        case 4:
+                            subTotal += mat4;
+                            break;
+                    }
+
+                    Console.WriteLine("Desea inscribirse en mas materias?");
+                    Console.WriteLine("1- Si");
+                    Console.WriteLine("2- No");
+                    continuar = Convert.ToInt16(Console.ReadLine());
+                    Console.Clear();
+                }
+
+                Console.WriteLine("Seleccione modo de pago: ");
+                Console.WriteLine("1- Un solo pago (15% de Descuento)");
+                Console.WriteLine("2- En 5 cuotas");
+
+                if (Console.ReadLine() == "1")
+                {
+                    descuento = true;
+                }
+                else
+                {
+                    descuento = false;
+                }
+
+                Console.Clear();
+
+                if (descuento == true)
+                {
+                    total = subTotal * 5 * 0.85;
+                    Console.WriteLine($"Usted paga en un solo pago: {total} (Descuento ya aplicado)");
+                }
+                else
+                {
+                    total = subTotal * 5;
+                    Console.WriteLine($"Usted paga 5 cuotas de ${subTotal} que hacen un total de ${total} al finalizar el cuatrimestre");
+                }
+                Console.ReadKey();
+                Console.Clear();
+                
+                Console.WriteLine("1- Volver al menu principal");
+                Console.WriteLine("2- Salir");
+                continuar = Convert.ToInt16(Console.ReadLine());
+                Console.Clear();
+                
+            }
+            Console.WriteLine("Hasta luego!");
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
